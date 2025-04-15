43107e53c46cc457786a46292937d2a477a98eb9
Julia Olshanska <yulyaolshanska@gmail.com>
2025-04-15 17:16:25 +0300

    feat: db config

---
+++ apps/backend/src/db/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+
+import { User, Product, Order } from '../entities/entities';
+import { DbConfig } from './db-config';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: DbConfig.DB_HOST,
+  port: DbConfig.DB_PORT,
+  username: DbConfig.DB_USER,
+  password: DbConfig.DB_PASS,
+  database: DbConfig.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [User, Product, Order],
+});
