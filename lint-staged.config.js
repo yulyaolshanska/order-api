3b6716252177a34f261abb619b34c9958902596c
Julia Olshanska <yulyaolshanska@gmail.com>
2025-04-16 21:43:14 +0300

    feat: remove unnecessary packages

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,8 @@
 /** @type {import('lint-staged').Config} */
 const config = {
-  "*": [() => "npm run lint:editor", () => "npm run lint:fs"],
+  "*": [
+    () => "npm run lint:editor",
+    () => "npm run lint:fs"],
 };
 
 export default config;
